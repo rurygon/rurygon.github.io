45eecea94d6e1fbb356a7ed2a291fa76236f2f29
rurygon <rury.gon@gmail.com>
2017-09-21 16:29:39 -0500

    set 'push' to 'get'

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "rury.gon@gmail.com";
 $subject = "Contact Form";
