e46d27dc3750766f2ad3c5764455fc0647413e1d
Rury Gonzalez <rury.gon@gmail.com>
2017-09-22 21:48:49 -0500

    update folder struct

---
+++ _js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+	
+	$("div").removeClass("header");
+	
+	$(".barLinks").mouseenter(function() {
+		$(this).fadeTo('fast', .5);
+	});
+	$(".barLinks").mouseleave(function() {
+		$(this).fadeTo('fast', 1);
+	});
+	
+	$("input[type='submit']").mouseenter(function() {
+		$(this).animate({
+			backgroundColor: 'black',
+			color: 'white'
+		}, "fast");
+	});
+	$("input[type='submit']").mouseleave(function() {
+		$(this).animate({
+			backgroundColor: 'white',
+			color: 'black'
+		}, "fast");
+	});
+	
+	$("input[type='reset']").mouseenter(function() {
+		$(this).animate({
+			backgroundColor: 'black',
+			color: 'white'
+		}, "fast");
+	});
+	$("input[type='reset']").mouseleave(function() {
+		$(this).animate({
+			backgroundColor: 'white',
+			color: 'black'
+		}, "fast");
+	});
+		
+	$('#contactForm').on('submit', function(e) {
+	  $('#contactForm *').fadeOut(2000);
+	  $('#contactForm').prepend('Your submission has been processed...');
+	});
+	
+	var windowHeight = $('.homeContact').height() - 75;
+	
+	$("#desktopStage").scroll(function (){
+		
+		if ($(this).scrollTop() > (windowHeight)) {
+			$('#headerFooter', "#desktopStage").addClass('header');
+			$('#headerFooter', "#desktopStage").removeClass('footer');
+		} 
+		else {
+			$('#headerFooter', "#desktopStage").removeClass('header');
+			$('#headerFooter', "#desktopStage").addClass('footer');
+		}
+	});
+	
+	var pageHeight = $('.page').height();
+	
+	$("a[href='#homeLink']").click(function() {
+		$("#desktopStage").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+	
+	$("a[href='#aboutLink']").click(function() {
+		$("#desktopStage").animate({ scrollTop: windowHeight }, "slow");
+		return false;
+	});
+	
+	$("a[href='#projectLink']").click(function() {
+		$("#desktopStage").animate({ scrollTop: windowHeight+pageHeight }, "slow");
+		return false;
+	});
+	
+	$("a[href='#contactLink']").click(function() {
+		$("#desktopStage").animate({ scrollTop: windowHeight+(pageHeight*2)+75 }, "slow");
+		return false;
+	});
+});
