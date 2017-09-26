9f7555906c6e37e25898cb91cf961196b8309bbc
Rury Gonzalez <rury.gon@gmail.com>
2017-09-25 22:22:57 -0500

    added mobile nav

---
+++ javascript/aboutPage.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	
+	$('.mobMenu').hide();
+	
+	$('.homeButton').click(function(){
+		$('.mobMenu').fadeToggle();
+	});
+	
+	$('#mobHome').click(function(){
+		window.location.href="/index.html"
+	});
+	
+	$('#mobProject').click(function(){
+		window.location.href="/pages/projectPage.html"
+	});
+	
+	$('#mobContact').click(function(){
+		window.location.href="/pages/contactPage.html"
+	});
+	
+});
