4bc14033b6e703160d1f892476ac0a222987beca
Rury Gonzalez <rury.gon@gmail.com>
2017-11-19 13:36:11 -0600

    mobile contact form

--- javaScript/contactPage.js
+++ javaScript/contactPage.js
@@ -1,31 +1,63 @@
 $(document).ready(function () {
 	
-	$("input[type='submit']").mouseenter(function() {
-		$(this).animate({
-			backgroundColor: 'black',
-			color: 'white'
-		}, "fast");
-	});
+	var viewWidth = $(window).width();
 	
-	$("input[type='submit']").mouseleave(function() {
-		$(this).animate({
-			backgroundColor: 'white',
-			color: 'black'
-		}, "fast");
-	});
-	
-	$("input[type='reset']").mouseenter(function() {
-		$(this).animate({
-			backgroundColor: 'black',
-			color: 'white'
-		}, "fast");
-	});
-	$("input[type='reset']").mouseleave(function() {
-		$(this).animate({
-			backgroundColor: 'white',
-			color: 'black'
-		}, "fast");
-	});
+	if (viewWidth < 768) {
+		$("input[type='submit']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+
+		$("input[type='submit']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+
+		$("input[type='reset']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+		$("input[type='reset']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+	}
+	else {
+		$("input[type='submit']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+
+		$("input[type='submit']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+
+		$("input[type='reset']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+		$("input[type='reset']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+	}
 	
 	$("#submitConfirm").hide();
 	
