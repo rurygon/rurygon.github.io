3a27d0837bf65971dbc0939a1eff34f8d757377e
Rury Gonzalez <rury.gon@gmail.com>
2017-11-20 16:44:21 -0600

    mobile optimization

--- javaScript/filmPage.js
+++ javaScript/filmPage.js
@@ -2,17 +2,15 @@ $(document).ready(function () {
 	
 	var viewWidth = $(window).width();
 	//JS for desktop only
-	if (viewWidth > 768) {
+	if (viewWidth < 768) {
+		
+	} else {
 		//Fades picture when user hovers
 		$(".pictureContainer > img").mouseenter(function() {
 			$(this).animate({borderRadius: '0%'}, "fast");
 		});
 		$(".pictureContainer > img").mouseleave(function() {
 			$(this).animate({borderRadius: '50%'}, "fast");
-		});
-	} else {
-		$(".pictureContainer > img").click(function() {
-			$(this).toggle("scale", {percent: 200}, "slow");
 		});
 	}
 
