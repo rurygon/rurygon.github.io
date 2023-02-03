8e373af93b07d2243623466ac5d60642c3a2511b
rurygon <rury.gon@gmail.com>
2023-02-03 17:43:33 -0600

    Add files via upload

---
+++ contactPage.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+	
+	var viewWidth = $(window).width();
+	
+	if (viewWidth < 768) {
+		$("input[type='submit']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+
+		$("input[type='submit']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+
+		$("input[type='reset']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+		$("input[type='reset']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+	}
+	else {
+		$("input[type='submit']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+
+		$("input[type='submit']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+
+		$("input[type='reset']").mouseenter(function() {
+			$(this).animate({
+				backgroundColor: 'black',
+				color: 'white'
+			}, "fast");
+		});
+		$("input[type='reset']").mouseleave(function() {
+			$(this).animate({
+				backgroundColor: 'white',
+				color: 'black'
+			}, "fast");
+		});
+	}
+	
+	$("#submitConfirm").hide();
+	
+	$('#contactForm').on('submit', function(e) {
+		$('#contactForm *').fadeOut(1000).delay(1000);
+		$("#submitConfirm").fadeIn(1000);
+	});
+	
+});
